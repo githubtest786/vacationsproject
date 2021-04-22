da4aff07e548e83f3c3c5a22d6f6ca5c5fb1571d
Armados <predomelo@gmail.com>
2021-04-23 01:04:45 +0300

    build

---
+++ precache-manifest.56a649235b8deb3442864bb4ef0c4cbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdd52bbb1a75332b41d4a4b97966133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b717b4137e594092617c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "088807ef6b0790e74791",
+    "url": "/static/css/main.22d531f3.chunk.css"
+  },
+  {
+    "revision": "b717b4137e594092617c",
+    "url": "/static/js/2.b28e3e2c.chunk.js"
+  },
+  {
+    "revision": "d0f75ef6f0422d17bba4e69e0f0956e4",
+    "url": "/static/js/2.b28e3e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088807ef6b0790e74791",
+    "url": "/static/js/main.f85eab75.chunk.js"
+  },
+  {
+    "revision": "46b8605b8114ead6a73e",
+    "url": "/static/js/runtime-main.5d97bd71.js"
+  }
+]);
